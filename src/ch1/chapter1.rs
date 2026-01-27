75da3af6033bc287f7204d9c81244b4eeeb1a010
Ioan Andrasi <ioan.andrasi@zf.com>
2026-01-27 14:09:11 +0200

    join() waits for threads to finish

--- src/ch1/chapter1.rs
+++ src/ch1/chapter1.rs
@@ -5,10 +5,16 @@ use std::thread;
     Basic example of spawn thread
 */
 pub fn spawn_thread() {
-    thread::spawn(f);
-    thread::spawn(f);
+    let t1 = thread::spawn(f);
+    let t2 = thread::spawn(f);
 
     println!("Hello from the main thread.");
+
+    /*
+        .join() will wait for threads to finish even if main finish first.
+     */
+    t1.join().unwrap();
+    t2.join().unwrap();
 }
 
 fn f() {
