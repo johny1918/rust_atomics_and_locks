d4d62e4068e25f5d8950276d50ba1bcfe700263a
Ioan Andrasi <andrasi.ioan90@gmail.com>
2026-02-04 21:03:08 +0200

    example of memory order relaxed

---
+++ src/ch3/mod.rs
@@ -0,0 +1,7 @@
+pub mod chapter_three;
+
+use chapter_three::happens_before_relationship;
+
+pub fn chapter_three_example_container() {
+    happens_before_relationship();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod ch1;
 mod ch2;
+mod ch3;
 
 use crate::ch1::chapter_one_example_container;
 use crate::ch2::chapter_two_example_container;
+use crate::ch3::chapter_three_example_container;
 
 
 fn main() {
     //chapter_one_example_container();
-    chapter_two_example_container();
+    //chapter_two_example_container();
+    chapter_three_example_container();
 }
