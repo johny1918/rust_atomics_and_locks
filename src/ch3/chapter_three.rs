fc82825f15c4b97a64b49fa8108909571bb0addd
Ioan Andrasi <andrasi.ioan90@gmail.com>
2026-02-07 14:04:44 +0200

    example of release acquire

--- src/ch3/chapter_three.rs
+++ src/ch3/chapter_three.rs
@@ -1,9 +1,13 @@
 use std::thread;
-use std::sync::atomic::AtomicI32;
-use std::sync::atomic::Ordering::Relaxed;
+use std::sync::atomic::{AtomicBool,  AtomicU64, AtomicI32};
+use std::sync::atomic::Ordering::{Relaxed, Acquire, Release};
+use std::time::Duration;
 
 static X: AtomicI32 = AtomicI32::new(0);
 static Y: AtomicI32 = AtomicI32::new(0);
+
+static DATA: AtomicU64 = AtomicU64::new(0);
+static READY: AtomicBool = AtomicBool::new(false);
 
 
 //happens-before relationship
@@ -31,4 +35,18 @@ fn b() {
     let y = Y.load(Relaxed);
     let x = X.load(Relaxed);
     println!("{} {}", x, y);
+}
+
+//release and acquire ordering
+pub fn release_and_aquire() {
+    thread::spawn(|| {
+        DATA.store(123, Relaxed);
+        READY.store(true, Release); // everything from before this store
+    });
+
+    while !READY.load(Acquire) { // is visible after this load 'true'
+        thread::sleep(Duration::from_millis(100));
+        println!("waiting");
+    }
+    println!("{}", DATA.load(Relaxed));
 }
--- src/ch3/mod.rs
+++ src/ch3/mod.rs
@@ -1,7 +1,9 @@
 pub mod chapter_three;
 
 use chapter_three::happens_before_relationship;
+use chapter_three::release_and_aquire;
 
 pub fn chapter_three_example_container() {
     happens_before_relationship();
+    release_and_aquire();
 }
